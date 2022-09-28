8401098189ebd2ad5ec83412a53456bf343eb1e8
Daniel Duran <danieldv96@gmail.com>
2022-09-28 00:36:29 -0500

    scrapy completed

--- fandom_league/fandom_league/spiders/fandom_league.py
+++ fandom_league/fandom_league/spiders/fandom_league.py
@@ -2,58 +2,114 @@ import scrapy
 # links
 # primera columna equipos response.xpath('//table[@class="wikitable hoverable-multirows md-table popup-window-container-y"]//span[@class="teamname"]/text()').getall()
 # segunda columna puntaje de equipos response.xpath('//table[@class="wikitable hoverable-multirows md-table popup-window-container-y"]//td[@rowspan="1" and @class=""]/text()').getall()
-# title response.xpath('//table[@class="wikitable hoverable-multirows md-table popup-window-container-y"]//th[@colspan="14"]/text()').getall()
+# title response.xpath('//table[@class="wikitable hoverable-multirows md-table popup-window-container-y"]//th[(@colspan="14") or (@colspan="13")]/text()').getall()
 # blueside and redside response.xpath('//table[@class="wikitable hoverable-multirows md-table popup-window-container-y"]//td[not(@rowspan) and (@class="" or @class="md-winner")]/text()').getall()
 # patch = response.xpath('//div[@id="matchlist"]//span[@class="matchlist-patches"]/span/a/text()').getall()
 #seasons = response.xpath('//div[@class="tabheader-tab"]/div[@class="tabheader-content"]/a[contains(@href,"Spring_Season") or contains(@href,"Summer_Season")]/@href').getall()
 # years = response.xpath('//div[@class="hlist"]//span/a[contains(@href,"LEC")]/@href').getall()
+# season title = response.xpath('//table[@class="infobox InfoboxTournament"]//th[@class="infobox-title"]/text()').get()
 
 
 class FandomLeague(scrapy.Spider):
     name = 'league'
-    start_urls = [
-        'https://lol.fandom.com/wiki/LEC/2020_Season'
-    ]
+
     custom_settings = {
-        'FEED_URI': 'lol.csv',
+        'FEED_URI': 'lol_2020_2022.csv',
         'FEED_FORMAT': 'csv',
         'FEED_EXPORT_ENCODING': 'utf-8',
+        'CONCURRENT_REQUESTS': 1,
         'ROBOTSTXT_OBEY': True
     }
-    follow_urls = ['https://lol.fandom.com/wiki/LEC/2021_Season',
-                   'https://lol.fandom.com/wiki/LEC/2022_Season']
 
-    def parse(self, response):
+    def start_requests(self):
+        yield scrapy.Request('https://lol.fandom.com/wiki/LEC/2020_Season/Spring_Season', self.parse_2020)
+        yield scrapy.Request('https://lol.fandom.com/wiki/LEC/2020_Season/Summer_Season', self.parse)
+        yield scrapy.Request('https://lol.fandom.com/wiki/LEC/2021_Season/Spring_Season', self.parse)
+        yield scrapy.Request('https://lol.fandom.com/wiki/LEC/2021_Season/Summer_Season', self.parse)
+        yield scrapy.Request('https://lol.fandom.com/wiki/LEC/2022_Season/Spring_Season', self.parse)
+        yield scrapy.Request('https://lol.fandom.com/wiki/LEC/2022_Season/Summer_Season', self.parse)
 
-        link_seasons = response.xpath(
-            '//div[@class="tabheader-tab"]/div[@class="tabheader-content"]/a[contains(@href,"Spring_Season") or contains(@href,"Summer_Season")]/@href').getall()
-        for season in link_seasons:
-
-            yield response.follow(season, callback=self.parse_link, cb_kwargs={'url': response.urljoin(season)})
-
-    def parse_link(self, response, **kwargs):
-        link = kwargs['url']
-        week = response.xpath(
-            '//table[@class="wikitable hoverable-multirows md-table popup-window-container-y"]//th[@colspan="14"]/text()').getall()
-        game_patch = response.xpath(
+    def parse_2020(self, response):
+        season = response.xpath(
+            '//table[@class="infobox InfoboxTournament"]//th[@class="infobox-title"]/text()').get()
+        weeks = response.xpath(
+            '//table[@class="wikitable hoverable-multirows md-table popup-window-container-y"]//th[(@colspan="14") or (@colspan="13")]/text()').getall()
+        patchs = response.xpath(
             '//div[@id="matchlist"]//span[@class="matchlist-patches"]/span/a/text()').getall()
 
-        first_team = response.xpath(
+        teams = response.xpath(
             '//table[@class="wikitable hoverable-multirows md-table popup-window-container-y"]//span[@class="teamname"]/text()').getall()
-        second_team = response.xpath(
+        score = response.xpath(
+            '//table[@class="wikitable hoverable-multirows md-table popup-window-container-y"]//td[@rowspan="1" and @class=""]/text()').getall()
+        numteam = 0
+        for num in range(0, len(score)):
+
+            yield {
+                'season': season,
+                'week': weeks[int((num/10)+1)],
+                'patch': patchs[int(num/10)],
+                'team_1_blue_side': teams[numteam],
+                'team_2_red_side': teams[numteam+1],
+                'score': score[num]
+            }
+            numteam += 2
+
+    def parse(self, response):
+        season = response.xpath(
+            '//table[@class="infobox InfoboxTournament"]//th[@class="infobox-title"]/text()').get()
+        weeks = response.xpath(
+            '//table[@class="wikitable hoverable-multirows md-table popup-window-container-y"]//th[(@colspan="14") or (@colspan="13")]/text()').getall()
+        patchs = response.xpath(
+            '//div[@id="matchlist"]//span[@class="matchlist-patches"]/span/a/text()').getall()
+
+        teams = response.xpath(
+            '//table[@class="wikitable hoverable-multirows md-table popup-window-container-y"]//span[@class="teamname"]/text()').getall()
+        score = response.xpath(
             '//table[@class="wikitable hoverable-multirows md-table popup-window-container-y"]//td[@rowspan="1" and @class=""]/text()').getall()
 
-        game_side = response.xpath(
-            '//table[@class="wikitable hoverable-multirows md-table popup-window-container-y"]//td[not(@rowspan) and (@class="" or @class="md-winner")]/text()').getall()
+        # as the page does not have an explicit way to show there is a super week, I needed to specify this for 2020 summer, 2021 and 2022 when super week was included
+        numteam = 0
 
-        yield {
-            'url': link,
-            'week': week,
-            'patch': game_patch,
-            'Team 1': first_team,
-            'team 2': second_team,
-            'Side': game_side
+        for num in range(0, len(score)):
+            if num < 15:
+                yield {
+                    'season': season,
+                    'week': weeks[1],
+                    'patch': patchs[0],
+                    'team_1_blue_side': teams[numteam],
+                    'team_2_red_side': teams[numteam+1],
+                    'score': score[num],
 
-        }
-        for follow_url in FandomLeague.follow_urls:
-            yield response.follow(follow_url, callback=self.parse)
+                }
+            elif 15 <= num < 75:
+
+                yield {
+                    'season': season,
+                    'week': weeks[int(((num-5)/10)+1)],
+                    'patch': patchs[int((num-5)/10)],
+                    'team_1_blue_side': teams[numteam],
+                    'team_2_red_side': teams[numteam+1],
+                    'score': score[num],
+                    'num': num
+                }
+            elif 75 <= num < 90:
+                yield {
+                    'season': season,
+                    'week': weeks[8],
+                    'patch': patchs[0],
+                    'team_1_blue_side': teams[numteam],
+                    'team_2_red_side': teams[numteam+1],
+                    'score': score[num],
+                }
+
+            elif 90 <= num:
+                yield {
+                    'season': season,
+                    'week': weeks[-1],
+                    'patch': patchs[-1],
+                    'team_1_blue_side': teams[numteam],
+                    'team_2_red_side': teams[numteam+1],
+                    'score': score[num],
+                }
+
+            numteam += 2
